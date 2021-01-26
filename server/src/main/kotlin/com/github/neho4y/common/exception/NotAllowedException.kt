bb0667697015700d3344adb6123f0091ea1d4840
Andrey Chausov <dxahtepb@gmail.com>
2021-01-26 22:19:40 +0300

    Add exceptions status

--- server/src/main/kotlin/com/github/neho4y/common/exception/NotAllowedException.kt
+++ server/src/main/kotlin/com/github/neho4y/common/exception/NotAllowedException.kt
@@ -1,3 +1,7 @@
 package com.github.neho4y.common.exception
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(code = HttpStatus.FORBIDDEN)
 class NotAllowedException(message: String = "Not allowed") : BasicException(message)
