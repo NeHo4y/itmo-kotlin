4f4b19b098001d19ea52baa0a05c6b4b5c80d895
Andrey Chausov <dxahtepb@gmail.com>
2021-01-26 05:34:18 +0300

    РУССКИЕ ВПЕРДЕ!!!!

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,64 +1,77 @@
 INSERT INTO USER (ID, CREATED_DATE, EMAIL, IS_DELETED, IS_EMAIL_VALID, PASSWORD, PHONE, ROLE, USERNAME)
-VALUES (user_seq.nextval, current_date(), 'admin1@qwe', false, true,
+VALUES (user_seq.nextval, CURRENT_TIME(), 'admin1@qwe', false, true,
         '$2a$10$NNckB4EA6svjF7MX6kgS0./oGg9bwwZBzotBkKMFOAMzggMzbmo9O', +15551234567, 'ADMIN', 'admin1'),
-       (user_seq.nextval, current_date(), 'admin2@qwe', false, true,
+       (user_seq.nextval, CURRENT_TIME(), 'admin2@qwe', false, true,
         '$2a$10$NNckB4EA6svjF7MX6kgS0./oGg9bwwZBzotBkKMFOAMzggMzbmo9O', +15551234567, 'ADMIN', 'admin2'),
-       (user_seq.nextval, current_date(), 'user3@qwe', false, true,
+       (user_seq.nextval, CURRENT_TIME(), 'user3@qwe', false, true,
         '$2a$10$NNckB4EA6svjF7MX6kgS0./oGg9bwwZBzotBkKMFOAMzggMzbmo9O', +15551234567, 'USER', 'user3'),
-       (user_seq.nextval, current_date(), 'user4@qwe', false, true,
+       (user_seq.nextval, CURRENT_TIME(), 'user4@qwe', false, true,
         '$2a$10$NNckB4EA6svjF7MX6kgS0./oGg9bwwZBzotBkKMFOAMzggMzbmo9O', +15551234567, 'USER', 'user4');
 
 INSERT INTO CATEGORY (ID, DESCRIPTION, IS_DELETED)
-VALUES (category_seq.nextval, 'РЈС‡РµС‚РЅР°СЏ Р·Р°РїРёСЃСЊ', false);
+VALUES (category_seq.nextval, 'Учетная запись', false);
 
 INSERT INTO TOPIC (ID, CATEGORY_ID, DESCRIPTION, IS_DELETED)
-VALUES (topic_seq.nextval, category_seq.currval, 'РћР±С‰РёРµ РІРѕРїСЂРѕСЃС‹', false);
+VALUES (topic_seq.nextval, category_seq.currval, 'Общие вопросы', false);
 
 INSERT INTO SUBTOPIC (ID, DESCRIPTION, IS_DELETED, TOPIC_ID)
-VALUES (subtopic_seq.nextval, 'РќРµС‚ РґРѕСЃС‚СѓРїР°', false, topic_seq.currval),
-       (subtopic_seq.nextval, 'РћР±СЉРµРґРёРЅРµРЅРёРµ СѓС‡РµС‚РЅС‹С… Р·Р°РїРёСЃРµР№', false, topic_seq.currval);
+VALUES (subtopic_seq.nextval, 'Нет доступа', false, topic_seq.currval),
+       (subtopic_seq.nextval, 'Объединение учетных записей', false, topic_seq.currval);
 
 INSERT INTO TOPIC (ID, CATEGORY_ID, DESCRIPTION, IS_DELETED)
-VALUES (topic_seq.nextval, category_seq.currval, 'РњР°РіР°Р·РёРЅ', false);
+VALUES (topic_seq.nextval, category_seq.currval, 'Магазин', false);
 
 INSERT INTO SUBTOPIC (ID, DESCRIPTION, IS_DELETED, TOPIC_ID)
-VALUES (subtopic_seq.nextval, 'РќРµРІРѕР·РјРѕР¶РЅРѕ Р·Р°Р№С‚Рё РІ РјР°РіР°Р·РёРЅ', false, topic_seq.currval),
-       (subtopic_seq.nextval, 'РћС€РёР±РєР° РїСЂРё РїРѕРєСѓРїРєРµ', false, topic_seq.currval);
+VALUES (subtopic_seq.nextval, 'Невозможно зайти в магазин', false, topic_seq.currval),
+       (subtopic_seq.nextval, 'Ошибка при покупке', false, topic_seq.currval);
 
 INSERT INTO CATEGORY (ID, DESCRIPTION, IS_DELETED)
-VALUES (category_seq.nextval, 'РћРїР»Р°С‚Р°', false);
+VALUES (category_seq.nextval, 'Оплата', false);
 
 INSERT INTO TOPIC (ID, CATEGORY_ID, DESCRIPTION, IS_DELETED)
-VALUES (topic_seq.nextval, category_seq.currval, 'РђРІС‚РѕСЂРёР·Р°С†РёСЏ РѕРїР»Р°С‚С‹', false);
+VALUES (topic_seq.nextval, category_seq.currval, 'Авторизация оплаты', false);
 
 INSERT INTO SUBTOPIC (ID, DESCRIPTION, IS_DELETED, TOPIC_ID)
-VALUES (subtopic_seq.nextval, 'РќРµР°РІС‚РѕСЂРёР·РёСЂРѕРІР°РЅРЅС‹Р№ РїР»Р°С‚РµР¶', false, topic_seq.currval),
-       (subtopic_seq.nextval, 'Р’РѕР·РјРµСЃС‚РёС‚СЊ РїР»Р°С‚РµР¶', false, topic_seq.currval);
+VALUES (subtopic_seq.nextval, 'Неавторизированный платеж', false, topic_seq.currval),
+       (subtopic_seq.nextval, 'Возместить платеж', false, topic_seq.currval);
 
 INSERT INTO TOPIC (ID, CATEGORY_ID, DESCRIPTION, IS_DELETED)
-VALUES (topic_seq.nextval, category_seq.currval, 'РћР±РЅРѕРІР»РµРЅРёРµ РёРЅС„РѕСЂРјР°С†РёРё РѕР± РѕРїР»Р°С‚Рµ', false);
+VALUES (topic_seq.nextval, category_seq.currval, 'Обновление информации об оплате', false);
 
 INSERT INTO SUBTOPIC (ID, DESCRIPTION, IS_DELETED, TOPIC_ID)
-VALUES (subtopic_seq.nextval, 'РќРµРІРѕР·РјРѕР¶РЅРѕ РѕР±РЅРѕРІРёС‚СЊ РёРЅС„РѕСЂРјР°С†РёСЋ', false, topic_seq.currval),
-       (subtopic_seq.nextval, 'РќРµРІРѕР·РјРѕР¶РЅРѕ РѕРїР»Р°С‚РёС‚СЊ РєР°СЂС‚РѕР№ РјРёСЂ', false, topic_seq.currval);
+VALUES (subtopic_seq.nextval, 'Невозможно обновить информацию', false, topic_seq.currval),
+       (subtopic_seq.nextval, 'Невозможно оплатить картой мир', false, topic_seq.currval);
+
 
 INSERT INTO FEEDBACK (ID, AUTHOR_ID, CATEGORY_ID, CREATION_DATE, END_DATE, HEADER, IS_ACTUAL, PRIORITY_ID, STATUS_ID, SUBTOPIC_ID, TOPIC_ID, UPDATE_DATE)
-VALUES (FEEDBACK_SEQ.nextval, 3, 1, current_date(), current_date(), 'РќРµС‚ РґРѕСЃС‚СѓРїР° Рє СѓС‡РµС‚РЅРѕР№ Р·Р°РїРёСЃРё', true, 'LOW', 'OPEN', 1, 1, current_date());
+VALUES (FEEDBACK_SEQ.nextval, 3, 1, CURRENT_TIME(), CURRENT_TIME(), 'Нет доступа к учетной записи', true, 'LOW', 'RESOLVED', 1, 1, CURRENT_TIME());
 
 INSERT INTO FEEDBACK_FOLLOWER (ID, FEEDBACK_ID, FOLLOWER_TYPE, USER_ID)
 VALUES (FOLLOWER_SEQ.nextval, FEEDBACK_SEQ.currval, 'ASSIGNEE', 2);
 
 INSERT INTO FEEDBACK_MESSAGE  (ID, AUTHOR_ID, IS_DELETED, IS_UNREAD, MESSAGE_DATE, MESSAGE_TEXT, MESSAGE_TYPE, FEEDBACK_ID)
-VALUES (FEEDBACK_MESSAGE_SEQ.nextval, 3, false, true, current_date(), 'РќРµ РјРѕРіСѓ РїРѕР»СѓС‡РёС‚СЊ РґРѕСЃС‚СѓРї Рє СѓС‡РµС‚РЅРѕР№ Р·Р°РїРёСЃРё', 'body', FEEDBACK_SEQ.currval),
-       (FEEDBACK_MESSAGE_SEQ.nextval, 2, false, true, current_date(), 'Рђ РІС‹ РїСЂРѕР±РѕРІР°Р»Рё РїРµСЂРµР·Р°РіСЂСѓР·РёС‚СЊ РєРѕРјРїСЊСЋС‚РµСЂ?', 'message', FEEDBACK_SEQ.currval),
-       (FEEDBACK_MESSAGE_SEQ.nextval, 3, false, true, current_date(), 'РџРѕРјРѕРіР»Рѕ', 'message', FEEDBACK_SEQ.currval);
+VALUES (FEEDBACK_MESSAGE_SEQ.nextval, 3, false, true, CURRENT_TIME(), 'Не могу получить доступ к учетной записи', 'body', FEEDBACK_SEQ.currval),
+       (FEEDBACK_MESSAGE_SEQ.nextval, 2, false, true, CURRENT_TIME(), 'А вы пробовали перезагрузить компьютер?', 'message', FEEDBACK_SEQ.currval),
+       (FEEDBACK_MESSAGE_SEQ.nextval, 3, false, true, CURRENT_TIME(), 'Помогло', 'message', FEEDBACK_SEQ.currval);
 
 
 INSERT INTO FEEDBACK (ID, AUTHOR_ID, CATEGORY_ID, CREATION_DATE, END_DATE, HEADER, IS_ACTUAL, PRIORITY_ID, STATUS_ID, SUBTOPIC_ID, TOPIC_ID, UPDATE_DATE)
-VALUES (FEEDBACK_SEQ.nextval, 4, 1, current_date(), current_date(), 'РћР±СЉРµРґРёРЅРµРЅРёРµ СѓС‡РµС‚РЅС‹С… Р·Р°РїРёСЃРµР№', true, 'MEDIUM', 'OPEN', 2, 1, current_date());
+VALUES (FEEDBACK_SEQ.nextval, 3, 2, CURRENT_TIME(), CURRENT_TIME(), 'Назначьте меня на админа', true, 'MEDIUM', 'CREATED', 5, 3, CURRENT_TIME());
 
 INSERT INTO FEEDBACK_MESSAGE  (ID, AUTHOR_ID, IS_DELETED, IS_UNREAD, MESSAGE_DATE, MESSAGE_TEXT, MESSAGE_TYPE, FEEDBACK_ID)
-VALUES (FEEDBACK_MESSAGE_SEQ.nextval, 4, false, true, current_date(), 'РџСЂРѕС€Сѓ РѕР±СЉРµРґРёРЅРёС‚СЊ СѓС‡РµС‚РЅС‹Рµ Р·Р°РїРёСЃРё
-* Р•Р“Р“РћР“
-* Р•Р“Р“РћР“', 'body', FEEDBACK_SEQ.currval),
-       (FEEDBACK_MESSAGE_SEQ.nextval, 1, false, true, current_date(), 'Р”Р° РєС‚Рѕ С‚Р°РєРѕР№ СЌС‚РѕС‚ РІР°С€ РµРіРіРѕРі?', 'message', FEEDBACK_SEQ.currval);
+VALUES (FEEDBACK_MESSAGE_SEQ.nextval, 3, false, true, CURRENT_TIME(), 'Да когда мой отзыв возьмут уже в работу???', 'body', FEEDBACK_SEQ.currval);
+
+
+INSERT INTO FEEDBACK (ID, AUTHOR_ID, CATEGORY_ID, CREATION_DATE, END_DATE, HEADER, IS_ACTUAL, PRIORITY_ID, STATUS_ID, SUBTOPIC_ID, TOPIC_ID, UPDATE_DATE)
+VALUES (FEEDBACK_SEQ.nextval, 4, 1, CURRENT_TIME(), CURRENT_TIME(), 'Объединение учетных записей', true, 'MEDIUM', 'IN_PROGRESS', 2, 1, CURRENT_TIME());
+
+INSERT INTO FEEDBACK_FOLLOWER (ID, FEEDBACK_ID, FOLLOWER_TYPE, USER_ID)
+VALUES (FOLLOWER_SEQ.nextval, FEEDBACK_SEQ.currval, 'ASSIGNEE', 1);
+
+INSERT INTO FEEDBACK_MESSAGE  (ID, AUTHOR_ID, IS_DELETED, IS_UNREAD, MESSAGE_DATE, MESSAGE_TEXT, MESSAGE_TYPE, FEEDBACK_ID)
+VALUES (FEEDBACK_MESSAGE_SEQ.nextval, 4, false, true, CURRENT_TIME(), 'Прошу объединить учетные записи
+* ЕГГОГ
+* ЕГГОГ', 'body', FEEDBACK_SEQ.currval),
+       (FEEDBACK_MESSAGE_SEQ.nextval, 1, false, true, CURRENT_TIME(), 'Да кто такой этот ваш еггог?', 'message', FEEDBACK_SEQ.currval),
+       (FEEDBACK_MESSAGE_SEQ.nextval, 4, false, true, CURRENT_TIME(), '> Да кто такой этот ваш еггог?
+Лучше вам и не знать, вы молодой, шутливый', 'message', FEEDBACK_SEQ.currval);
